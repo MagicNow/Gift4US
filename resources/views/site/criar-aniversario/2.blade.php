f881d0ba96e708c94269ad61b10a42d7494728fd
Heitor Glockner <mail@heitorglockner.com>
2017-07-21 12:51:55 -0300

    create step 3

--- resources/views/site/criar-aniversario/2.blade.php
+++ resources/views/site/criar-aniversario/2.blade.php
@@ -12,11 +12,11 @@
 						<fieldset class="form-birthday-first col-xs-12 col-sm-12 col-md-5 col-lg-5">
 							<div class="form-group">
 								<label for="aniver-endereco">Qual o endereço da festa?</label>
-								<input type="text" class="form-control form-input" id="aniver-endereco" name="endereco">
+								<input type="text" class="form-control form-input" id="aniver-endereco" name="endereco" placeholder="">
 							</div>
 							<div class="form-group">
 								<label for="aniver-referencia">Quer inserir um ponto de referência? (opcional)</label>
-								<input type="text" class="form-control form-input" id="aniver-referencia" name="referencia">
+								<input type="text" class="form-control form-input" id="aniver-referencia" name="referencia" placeholder="">
 							</div>
 							<div class="form-group">
 								<label for="aniver-observacoes">Observações gerais (opcional)</label>
@@ -36,7 +36,7 @@
 							<li class="form-birthday-paginate-item"></li>
 						</ul>
 					</nav>
-					<button type="button" name="enviar" class="form-birthday-submit"><img src="{{ asset('assets/site/images/niver-next-step.png') }}" alt="Próxima Etapa"></button>
+					<button type="submit" name="enviar" class="form-birthday-submit"><img src="{{ asset('assets/site/images/niver-next-step.png') }}" alt="Próxima Etapa"></button>
 				</form>
 			</div>
 		</div>
