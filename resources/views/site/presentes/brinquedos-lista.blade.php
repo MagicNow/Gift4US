6f60bd8ad109cc37375e2a1aa46baa63c33d421e
johndieg <diste11@gmail.com>
2017-10-31 17:39:27 -0200

    correcao modal finalizar lista

--- resources/views/site/presentes/brinquedos-lista.blade.php
+++ resources/views/site/presentes/brinquedos-lista.blade.php
@@ -1,7 +1,7 @@
 @extends('site/master')
 
 @section('content')
-	<div class="gifts-modal hidden">
+	<div class="gifts-modal modal-excluir-presente hidden">
 		<div class="gifts-modal-content col-md-3 col-md-offset-5">
 			<h3 class="gifts-modal-title">Excluir este presente da sua lista</h3>
 			<p class="gifts-modal-subtitle">Você realmente deseja excluir este presente?</p>
@@ -9,6 +9,19 @@
 			<div class="gifts-modal-buttons">
 				<button class="gifts-modal-button gifts-modal-button-remove">Excluir</button>
 				<button class="gifts-modal-button gifts-modal-button-cancel">Cancelar</button>
+			</div>
+		</div>
+	</div>
+	<div class="gifts-modal modal-lista-concluir hidden">
+		<div class="gifts-modal-content col-md-3 col-md-offset-5">
+			<h3 class="gifts-modal-title">Lista de Brinquedos</h3>
+			<p class="gifts-modal-subtitle">Parabéns! Você está perto de finalizar sua lista de brinquedos!</p>
+			<p class="gifts-modal-subtitle">
+				<img src="{{ asset('assets/site/images/presentinho-icone.png') }}">
+				<small class="gifts-box-number-header-total">99</small> selecionados
+			</p>
+			<div class="gifts-modal-buttons">
+				<a class="gifts-box-number-submit col-md-12" href="{{ route('usuario.meus-aniversarios.novo.festa', [ $party->id, 5 ]) }}">Concluir Lista</a>
 			</div>
 		</div>
 	</div>
