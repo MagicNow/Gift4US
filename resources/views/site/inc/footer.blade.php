d8e770a0f6b9d81a1a1fc77707b06fd50a8c1066
Heitor Glockner <mail@heitorglockner.com>
2017-11-08 23:50:21 -0200

    footer links

--- resources/views/site/inc/footer.blade.php
+++ resources/views/site/inc/footer.blade.php
@@ -5,26 +5,28 @@
 			{{ Html::image('assets/site/images/logofooter.png', '', array('class' => 'logo')) }}
 			<div class="nav col-lg-4 col-md-4 col-xs-12 col-sm-12" >
 				<ul class="col-lg-6 col-md-6 col-xs-12 col-sm-12">
-					<li><a href="#" alt="">aniversário convidado</a></li>
-					<li><a href="#" alt="">área do usuário</a></li>
-					<li><a href="{{ route('usuario.meus-aniversarios') }}" alt="">meus aniversários</a></li>
-					<li><a href="#" alt="">listas de presentes</a></li>
-					<li><a href="{{ URL::to('usuario/transferencia') }}" alt="">resgatar valores</a></li>
+					<li><a href="{{ route('home') . '#convidado_aniversariante' }}">aniversário convidado</a></li>
+					<li><a href="{{ route('home') . '#convidado_aniversariante' }}">área do usuário</a></li>
+					@if (Auth::user()->name)
+						<li><a href="{{ route('usuario.meus-aniversarios') }}">meus aniversários</a></li>
+						<li><a href="#">listas de presentes</a></li>
+						<li><a href="{{ URL::to('usuario/transferencia') }}">resgatar valores</a></li>
+					@endif
 				</ul>
 				<ul class="col-lg-6 col-md-6 col-xs-12 col-sm-12">
-					<li><a href="#" alt="">como funciona</a></li>
-					<li><a href="#" alt="">passo a passo</a></li>
-					<li><a href="#" alt="">dúvidas frequentes</a></li>
-					<li><a href="#" alt="">contato</a></li>
+					<li><a href="{{ route('home') . '#como_funciona' }}">como funciona</a></li>
+					<li><a href="{{ route('home') . '#passoapasso' }}">passo a passo</a></li>
+					<li><a href="#">dúvidas frequentes</a></li>
+					<li><a href="#">contato</a></li>
 				</ul>
 			</div>
 			{{ Html::image('assets/site/images/presentinho_red.png', '', array('class' => 'presentinho_red')) }}
 			{{ Html::image('assets/site/images/presentinho_blue.png', '', array('class' => 'presentinho_blue')) }}
 			<div class="placa col-lg-3 col-md-3 col-xs-12">
 				<ul>
-					<li><a href="mailto:contato@gift4us.com" alt="">{{ Html::image('assets/site/images/email.png', '', array('class' => '')) }} </a></li>
-					<li><a href="https://www.facebook.com/Gift4us-1534294213546524" target="_blank" title="Facebook" alt="">{{ Html::image('assets/site/images/fb.png', '', array('class' => '')) }} </a></li>
-					<li><a href="http://instagram.com" target="_blank title="Instagram" alt="">{{ Html::image('assets/site/images/insta.png', '', array('class' => '')) }} </a></li>
+					<li><a href="mailto:contato@gift4us.com">{{ Html::image('assets/site/images/email.png', '', array('class' => '')) }} </a></li>
+					<li><a href="https://www.facebook.com/Gift4us-1534294213546524" target="_blank" title="Facebook">{{ Html::image('assets/site/images/fb.png', '', array('class' => '')) }} </a></li>
+					<li><a href="http://instagram.com" target="_blank title="Instagram">{{ Html::image('assets/site/images/insta.png', '', array('class' => '')) }} </a></li>
 				</ul>
 			</div>
 		</div>
