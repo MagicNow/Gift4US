47381c5e0e94646f1d6b0934906d61b342ba6030
Heitor Glockner <mail@heitorglockner.com>
2017-11-20 23:20:14 -0200

    bug fix

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -79,7 +79,7 @@ class NotificationsController extends Controller
 		$toysTotal = $this->toys->count();
 		$this->toysAvalible = $this->toys->whereNull('nome');
 
-		return $this->toysAvalible->count() > 0 ? round(($this->toysAvalible->count() * 100) / $toysTotal, 0, PHP_ROUND_HALF_EVEN);
+		return $this->toysAvalible->count() > 0 ? round(($this->toysAvalible->count() * 100) / $toysTotal, 0, PHP_ROUND_HALF_EVEN) : 0;
 	}
 
 	public function conviteDigital(Request $request, $festa_id = null)
