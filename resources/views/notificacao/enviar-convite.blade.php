9f246bfbe3534f7689f5760341dcade57a00c8f8
Heitor Glockner <mail@heitorglockner.com>
2018-02-17 19:49:30 -0200

    send invite link

--- resources/views/notificacao/enviar-convite.blade.php
+++ resources/views/notificacao/enviar-convite.blade.php
@@ -27,7 +27,7 @@
 						<div class="row enviar-convite-secao">
 							<div class="col-md-7">
 								<h5>Link da página do aniversáriante</h5>
-								<span class="enviar-convite-texto" id="convite-url">{{ route('convidado.index', $party->id) }}</span>
+								<span class="enviar-convite-texto" id="convite-url">{{ route('convidado.index', $party->codigo) }}</span>
 								<div class="row">
 									<button type="button" class="copy-button enviar-convite-button col-md-6" data-clipboard-target="#convite-url"><span class="button">Copiar</span></button>
 								</div>
@@ -44,14 +44,14 @@
 							<div class="col-md-6 enviar-convite-coluna">
 								<h5 class="facebook">Compartilhar no Facebook</h5>
 								<div class="row">
-									<a href="https://www.facebook.com/sharer/sharer.php?u={{ route('convidado.index', $party->id) }}" target="_blank" class="col-md-12 enviar-convite-button"><span class="button">Compartilhar<br>no perfil</span></a>
+									<a href="https://www.facebook.com/sharer/sharer.php?u={{ route('convidado.index', $party->codigo) }}" target="_blank" class="col-md-12 enviar-convite-button"><span class="button">Compartilhar<br>no perfil</span></a>
 								</div>
 							</div>
 							<div class="col-md-6 enviar-convite-coluna">
 								<h5 class="whatsapp">Compartilhar no Whatsapp</h5>
 								<div class="row">
-									<a href="whatsapp://send?text={{ url(Storage::url('public/birthdays/invites/' . $party->id . '.jpg')) }}" class="col-md-6 enviar-convite-button"><span class="button">Compartilhar convite</span></a>
-									<a href="whatsapp://send?text={{ route('convidado.index', $party->id) }}" class="col-md-6 enviar-convite-button"><span class="button">Compartilhar <br>link da página</span></a>
+									<a href="whatsapp://send?text={{ url(Storage::url('public/birthdays/invites/' . $party->codigo . '.jpg')) }}" class="col-md-6 enviar-convite-button"><span class="button">Compartilhar convite</span></a>
+									<a href="whatsapp://send?text={{ route('convidado.index', $party->codigo) }}" class="col-md-6 enviar-convite-button"><span class="button">Compartilhar <br>link da página</span></a>
 								</div>
 							</div>
 						</div>
