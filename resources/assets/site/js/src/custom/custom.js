5bbe1ef4f47329386ff349fc8be1cdc505a3f352
Heitor Glockner <mail@heitorglockner.com>
2018-02-19 22:35:13 -0300

    scroll

--- resources/assets/site/js/src/custom/custom.js
+++ resources/assets/site/js/src/custom/custom.js
@@ -782,76 +782,29 @@ $(function() {
             scrollDown++;
 		}
 
-		if(scrollDown > 0 && $htmlBody.scrollTop() <= previewSections[0].top) {
-			scrollDown = 0;
+		$sectionActive = $('.preview-section.active');
+
+		if(scrollUp >= scrollSensitive && $sectionActive.prev('.preview-section').length > 0) {
+			$htmlBody.animate({
+				scrollTop: $sectionActive.prev('.preview-section').offset().top
+			}, scrollTime);
+
+			scrollUp = 0;
+			$htmlBody.css('overflow', 'hidden'); 
 		} else {
-			if(scrollUp > 0 && $htmlBody.scrollTop() >= previewSections[previewSections.length - 1].top) {
-				scrollUp = 0;
-			}
-		}
-
-		/*
-		console.log(previewSections.length > 0, $htmlBody.scrollTop() >= previewSections[0].top, $htmlBody.scrollTop() <= previewSections[previewSections.length - 1].bottom);
-		if (previewSections.length > 0 && $htmlBody.scrollTop() >= previewSections[0].top && $htmlBody.scrollTop() <= previewSections[previewSections.length - 1].bottom) {
-			$htmlBody.css('overflow', 'hidden');
-
-
-
-			console.log('scrollDown', scrollDown >= scrollSensitive, $sectionActive.next('.preview-section').length > 0)
 			if(scrollDown >= scrollSensitive && $sectionActive.next('.preview-section').length > 0) {
 				$htmlBody.animate({
 					scrollTop: $sectionActive.next('.preview-section').offset().top
 				}, scrollTime);
 
 				scrollDown = 0;
+				$htmlBody.css('overflow', 'hidden');
 			} else {
-				console.log(scrollUp >= scrollSensitive , $sectionActive.prev('.preview-section').length > 0);
-				if(scrollUp >= scrollSensitive && $sectionActive.prev('.preview-section').length > 0) {
-					$htmlBody.animate({
-						scrollTop: $sectionActive.prev('.preview-section').offset().top
-					}, scrollTime);
-
-					scrollUp = 0;
-				}
-			}
-			*/
-
-
-		if ($htmlBody.scrollTop() > previewSections[0].top && $htmlBody.scrollTop() < previewSections[previewSections.length - 1].bottom) {
-			$htmlBody.css('overflow', 'hidden');
-		} else {
-			$htmlBody.css('overflow', 'auto');
-		}
-
-		$sectionActive = $('.preview-section.active');
-		if ($sectionActive.length > 0) {
-			if(scrollUp >= scrollSensitive && $sectionActive.prev('.preview-section').length > 0) {
-				$htmlBody.animate({
-					scrollTop: $sectionActive.prev('.preview-section').offset().top
-				}, scrollTime);
-
-				scrollUp = 0;
-			} else {
-				if(scrollUp >= scrollSensitive && $sectionActive.prev('.preview-section').length === 0) {
-					$htmlBody.animate({
-						scrollTop: $sectionActive.offset().top - $sectionActive.height() - 1
-					}, scrollTime);
+				if ($sectionActive.index() == 0 && scrollUp >= scrollSensitive) {
+					$htmlBody.css('overflow', 'auto');
 				} else {
-					if(scrollDown >= scrollSensitive && $sectionActive.next('.preview-section').length > 0) {
-						$htmlBody.animate({
-							scrollTop: $sectionActive.next('.preview-section').offset().top
-						}, scrollTime);
-
-						scrollDown = 0;
-					} else {
-						if(scrollDown >= scrollSensitive && $sectionActive.next('.preview-section').length === 0) {
-							// $htmlBody.animate({
-							// 	scrollTop: $sectionActive.offset().top + $sectionActive.height()
-							// }, scrollTime);
-	
-							// scrollDown = 0;
-							$htmlBody.css('overflow', 'auto');
-						}
+					if ($sectionActive.index() == $sections.length - 1 && scrollDown >= scrollSensitive) {
+						$htmlBody.css('overflow', 'auto');
 					}
 				}
 			}
