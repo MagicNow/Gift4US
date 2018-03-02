4eacc468df1a3290bce8d99a65759b69519344e5
Heitor Glockner <mail@heitorglockner.com>
2018-03-01 23:58:11 -0300

    send mail

--- resources/views/emails/invite/send.blade.php
+++ resources/views/emails/invite/send.blade.php
@@ -6,7 +6,7 @@
 	<title>Convite</title>
 </head>
 <body style="margin: 0; padding: 0;">
-	<table cellspacing="0" width="562" cellpadding="0" border="0" style="text-align: center; margin: 0 auto; display: block;">
+	<table cellspacing="0" width="800" cellpadding="0" border="0" style="text-align: center; margin: 0 auto; display: block;">
 		<tr>
 			<td align="center">
 				<img src="{{ url(Storage::url('public/birthdays/invites/' . $festa . '.jpg')) }}" />
