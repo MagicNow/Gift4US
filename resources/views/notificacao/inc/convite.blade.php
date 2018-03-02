aa6fc15f3008d6831c9582ad048d7e0d9d5ce95c
Heitor Glockner <mail@heitorglockner.com>
2018-03-02 00:41:43 -0300

    invite columns

--- resources/views/notificacao/inc/convite.blade.php
+++ resources/views/notificacao/inc/convite.blade.php
@@ -9,24 +9,24 @@
 				Festa de {{ isset($party->idade_anos) && $party->idade_anos > 0 ? $party->idade_anos . ' anos' : NULL }} {{ isset($party->idade_meses) && $party->idade_meses > 0 ? $party->idade_meses . ' meses' : NULL }} de<br /><span>{{ $party->nome }}</span>
 			</p>
 		</div>
-		<div class="clearfix">
-			<div class="col-md-4 col-xs-4">
+		<div class="clearfix convite-detalhes">
+			<div class="col-md-4 col-xs-4 convite-detalhes-left">
 				<div class="passos">
-					<div class="passo1">
+					<div class="passo1 convite-detalhes-passo">
 						<div class="bg-presente">1</div>
 						<span>entre no site www.gift4us.com.br</span>
 					</div>
-					<div class="passo2">
+					<div class="passo2 convite-detalhes-passo">
 						<div class="bg-presente">2</div>
 						<span>insira o aniversário ou o código <strong>{{ $party->codigo }}</strong></span>
 					</div>
-					<div class="passo3">
+					<div class="passo3 convite-detalhes-passo">
 						<div class="bg-presente">3</div>
 						<span>confira a lista de presentes {{ $party->confirma_presenca === 0 ? '' : 'e confirme sua presença' }}</span>
 					</div>
 				</div>
 			</div>
-			<div class="col-md-8 col-xs-8">
+			<div class="col-md-8 col-xs-8 convite-detalhes-right">
 				<div class="row convite-content">
 					<div class="data-festa">
 						<span>{{ sprintf('%02d', $party->festa_dia) }}/{{ sprintf('%02d', $party->festa_mes) }}/{{ $party->festa_ano }} {{ sprintf('%02d', $party->festa_hora) }}h{{ sprintf('%02d', $party->festa_minuto) }}</span>
