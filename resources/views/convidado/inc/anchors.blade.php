2c993b2ddb59d2ab546bc5e149bc9a316e7e2ee3
Heitor Glockner <mail@heitorglockner.com>
2018-03-05 03:35:36 -0300

    top

--- resources/views/convidado/inc/anchors.blade.php
+++ resources/views/convidado/inc/anchors.blade.php
@@ -1,4 +1,4 @@
-<div class="sub-menu text-center">
+<div class="sub-menu text-center" id="topo">
 	<a class="confirm-btn" href="{{ route('convidado.index', $party->codigo) }}#confirmar"><img src="{{ asset('assets/site/images/img-check-out.png') }}" alt="" /></a>
 	<a class="gifts-btn active" href="{{ route('convidado.index', $party->codigo) }}#lista"><img src="{{ asset('assets/site/images/img-presente-in.png') }}" alt="" /></a>
 	<a class="message-btn" href="{{ route('convidado.index', $party->codigo) }}#recado"><img src="{{ asset('assets/site/images/img-mensagem-out.png') }}" alt="" /></a>
