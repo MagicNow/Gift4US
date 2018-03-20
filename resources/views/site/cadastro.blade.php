a4afe2105943996a8c94354a8321a54595790d08
Heitor Glockner <mail@heitorglockner.com>
2018-03-19 22:39:22 -0300

    termos de uso

--- resources/views/site/cadastro.blade.php
+++ resources/views/site/cadastro.blade.php
@@ -1,6 +1,101 @@
 @extends('site/master')
 
 @section('content')
+	<div class="modal fade" id="modal-termos" tabindex="-1" role="dialog" aria-labelledby="modal-termosLabel" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="modal-termosLabel">Termos de uso e condições de navegação do portal</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<p>A Gift4Us apresenta aqui os Termos de Uso e Condições de Navegação do site da Gift4Us que deve ser respeitado pelo internauta, sob pena de todas as implicações da legislação em vigor.<br>
+					Este Portal é destinado a proporcionar ao internauta maior comodidade e agilidade na criação de listas de presentes para festas infantis, além da possibilidade de montar seu próprio site<br>
+					Para os fins deste termo de uso, devem ser consideradas as seguintes definições e descrições para seu melhor entendimento:<br>
+					Gift4Us: denominação utilizada nestes Termos de Uso e Condições de Navegação para identificar a detentora e proprietária deste Portal, Gift4Us, pessoa jurídica de direito privado, inscrita sob CNPJ nº 26.155.459/0001-58 com sede Rua Doutor Tomas Carvalhal, Paraiso, 728, sala 402, CEP 04006-002, na Cidade de São Paulo, Estado de São Paulo</p>
+					
+					<p>Lista de presentes virtuais: Funcionalidade que propicia a criação de uma lista de presentes em que os USUÁRIOS desejam ganhar dinheiro como presentes de aniversário e que permite aos CONVIDADOS comprarem através das formas de pagamento oferecidas pelo Gift4Us as cotas de presentes fictícios.<br>
+					Cotas: possibilita dividir o valor de determinado presente em cotas de igual valor, permitindo aos Convidados comprarem e presentearem os Usuários com uma ou mais cota(s).<br>
+					Login: Identificação de um usuário perante o sistema. Para ter acesso, é possível utilizar seu e-mail pessoal e senha previamente cadastrado ou a utilização de dados pessoais de uma conta do Facebook.<br>
+					Senha: Conjunto de caracteres previamente convencionado, utilizado no processo de verificação da identidade do USUÁRIO, assegurando que ele é realmente o usuário responsável pela sua conta e pelo Site de Aniversário.<br>
+					Usuário: Qualquer pessoa física que tenha feito seu cadastro no Gift4Us, por livre e espontânea vontade e use ou pretenda usar as ferramentas colocadas à sua disposição.<br>
+					Convidados: Qualquer pessoa física que faça seu acesso como Convidado e use ou pretenda usar as ferramentas colocadas à sua disposição pelo Gift4Us.<br>
+					Evento: Qualquer celebração que pretenda ser organizada e divulgada pelo(s) Usuário(s) com o auxílio das funcionalidades e ferramentas disponibilizadas pelo Gift4Us.<br>
+					Fornecedores: Qualquer pessoa física ou jurídica que ofereça serviços relacionados à organização de eventos ou à venda de presentes, cujos serviços sejam divulgados na plataforma Gift4Us ou que tenha feito seu cadastro como Fornecedor.<br>
+					Portal: Qualquer página contida dentro do domínio gift4us.com.br.<br>
+					Taxas e Serviços: Do valor arrecadado com presentes virtuais, cujos pagamentos tenham sido realizados por empresas de pagamento online, será cobrada uma taxa de 8,99% (oito inteiros e noventa e nove centésimos percentuais) do valor total no pedido e 3,50% (três inteiros e cinquenta centésimos percentuais) via boleto bancário, no momento do pedido do resgate.</p>
+					
+					<p>Aceitação dos Termos de uso<br>
+					Para navegar em algumas páginas do Gift4Us, o Usuário não precisará ser cadastrado, nem precisará confirmar sua aceitação aos Termos de Uso. Isso, contudo, não significa que o Usuário não esteja sujeito aos Termos de Uso, cuja aceitação decorre do próprio acesso ao portal.<br>
+					Para usufruir dos serviços oferecidos pelo Gift4Us, o Usuário deverá primeiro efetuar seu cadastro diretamente no portal do Gift4Us fornecendo seu e-mail e cadastrando uma senha ou utilizar-se de cadastro previamente feito no Facebook o que implicará a aceitação dos Termos de Uso.<br>
+					A contratação do Gift4Us é formalizada pelo preenchimento do formulário de cadastro disponibilizado no portal e é regulada por estes Termos de Uso, bem como pelas Políticas de Privacidade.<br>
+					O Usuário concorda em usar os Serviços prestados pelo Gift4Us somente para os fins permitidos.<br>
+					O Usuário obriga-se a não violar quaisquer leis, regulamentos ou costumes geralmente aceitos no Brasil ao se utilizar das Ferramentas e dos Serviços.<br>
+					O Usuário está ciente de que, ao aceitar estes termos de uso, concorda e autoriza o recebimento de material de empresas parceiras do Gift4Us, como por exemplo boletins informativos (newsletters).<br>
+					As informações sobre organização de Eventos são oferecidas aos Usuários de forma não onerosa. Algumas outras funcionalidades e serviços são oferecidos de forma não onerosa ou mediante pagamento, conforme indicado no site.<br>
+					Os Usuários e os Convidados compreendem que a ferramenta de organização e divulgação de lista de presentes virtuais não corresponde à efetiva compra dos produtos ilustrados e sua entrega física aos Usuários, propiciando, ao contrário, o recebimento em dinheiro dos valores pagos pelos Convidados.<br>
+					É vedada aos Usuários a utilização do Gift4Us para finalidades comerciais, publicitárias ou qualquer outra que contrarie a realidade para o qual foi concebido, conforme definido nos Termos de Uso.<br>
+					A Gift4Us apenas se responsabiliza pela conexão do internauta com os estabelecimentos e links que oferecem os serviços, não sendo-lhe imputável caso ocorra algum erro, pois foge da sua esfera de atuação e interferência.<br>
+					A Gift4Us apenas fornece os links que acredita ter relevância ao usuário, sendo assim, não se responsabiliza pela entrega nem pelo estado dos produtos adquiridos pelos usuários. </p>
+					
+					<p>
+						Cadastro, Login, Senha e Segurança<br>
+						Qualquer pessoa maior de 18 (dezoito) anos pode utilizar livremente os serviços do Gift4Us. NÃO É AUTORIZADA a participação de usuários menores de idade. Caso seja verificada eventual infração desta disposição, O Gift4Us cancelará, de imediato, o cadastro do Usuário.<br>
+						Quando o Usuário acessar o Gift4Us utilizando-se de seu cadastro, previamente criado através do nosso portal ou através do Facebook, deverá lembrar que seu login e senha passarão a ser sua identificação perante o Gift4Us. É por meio desses dados que o Usuário pode acessar e alterar suas informações pessoais e outros dados de seu cadastro.<br>
+						O Usuário será o único responsável por seu login e senha e responderá por todos os atos praticados com estes. Portanto, é dever do Usuário zelar pela guarda e confidencialidade de sua senha.<br>
+						O internauta deverá efetuar cadastro no presente Portal se desejar adquirir produtos ou contratar seus serviços, fornecendo seus dados pessoais conforme requisitado e adotando uma senha forte para sua identificação pessoal.<br>
+						Quando o internauta realiza o cadastro neste Portal deve fornecer somente informações fiéis e verdadeiras, se comprometendo a partir de então, a manter seus dados sempre atualizados, sob pena de responder civil e criminalmente além de arcar com os danos ocasionados pela utilização de dados incorretos, ou de terceiros, independentemente da existência de culpa.<br>
+						Os dados requeridos pelo Portal Gift4Us são:
+					</p>
+					<ol>
+						<li>Nome e Sobrenome do responsável</li>
+						<li>E-mail do responsável</li>
+						<li>Telefone fixo e celular do responsável</li>
+						<li>RG e CPF do responsável</li>
+						<li>Dados bancários do responsável</li>
+						<li>Data de nascimento do aniversariante</li>
+						<li>Nome e Sobrenome do aniversariante</li>
+					</ol>
+					
+					<p>
+						O Usuário concorda em notificar imediatamente o Gift4Us sobre o extravio, perda, roubo ou qualquer uso não autorizado de seu login e senha, bem como sobre qualquer quebra de segurança de seu conhecimento. Diante dessas situações, constitui também obrigação do Usuário alterar imediatamente a sua senha após a ocorrência de tais fatos.<br>
+						O Gift4Us obriga-se a não utilizar as informações cadastrais fornecidas pelos Usuários para a realização de quaisquer atividades ilícitas, mas somente para aquelas expressamente permitidas pela legislação brasileira e/ou por estes Termos de Uso.<br>
+						Os Usuários concordam em:<br>
+						a) Fornecer informações verdadeiras, exatas, atuais e completas ao realizar seu cadastro no portal;<br>
+						b) Atualizar suas informações de forma a mantê-las verdadeiras, exatas e completas.<br>
+						c) Ser responsabilizado por qualquer informação falsa, incorreta, desatualizada ou incompleta fornecida.<br>
+						Caso o Gift4Us tenha razões suficientes para suspeitar da veracidade e/ou da exatidão de tais informações, terá o direito de suspender ou cancelar o cadastro do Usuário, imediatamente e independente de notificação. A suspensão ou o cancelamento, contudo, não cessa, nem diminui a responsabilidade do Usuário quanto à exatidão e veracidade dos dados que fornecer.<br>
+						É vedado ao Usuário a cessão, venda, aluguel ou qualquer outra forma de transferência de seu cadastro. Também não se permitirá a manutenção de mais de um cadastro por pessoa, ou ainda a criação de novos cadastros por pessoas cujos cadastros originais tenham sido cancelados por infrações aos Termos de Uso.<br>
+						Os Usuários reconhecem e concordam que os Convidados terão acesso às informações que forem cadastradas no Site do aniversário. <br>
+						Os Usuários obrigam-se a não fazer uso dos dados dos Convidados, inseridos na base de dados do Site do aniversário, para fins diversos aos que motivaram o seu cadastramento.<br>
+						Os Usuários poderão, a qualquer momento, solicitar o cancelamento de seus cadastros e a exclusão de seus dados e informações da base de dados do Gift4Us<br>
+						Os Usuários reconhecem que o encerramento de um cadastro e a exclusão definitiva das informações pode demorar até 90 (noventa) dias úteis para ser realizada.<br>
+						O Site do aniversário de uma pessoa falecida pode ser encerrado a pedido de algum de seus herdeiros ou representantes, desde que devidamente comprovado o falecimento através de documento hábil. Em caso de dúvidas sobre a proteção a dados pessoais ou para obter mais informações a respeito de como tratamos as informações que nos são fornecidas, consulte nossa Política de Privacidade.<br>
+						A navegação por diversas páginas do Gift4Us é livre.<br>
+						A contratação dos serviços de criação e hospedagem do Site do aniversário, com a possibilidades de usufruir de diversas funcionalidades para facilitar a organização e divulgação de seu Evento é gratuita.<br>
+						Todos os pagamentos serão arrecadados com a utilização do serviço de pagamentos online por meio de cartão de crédito e ou boleto bancário.<br>
+						Somente internautas que atendam às condições exigidas de cadastro no Portal da Gift4Us poderão efetuar compra.<br>
+						É condição essencial que o internauta preencha todos os dados corretamente para completar o procedimento de compra, quais sejam o nome do titular do cartão para pagamento, o C.P.F. do mesmo titular, número do cartão, data de validade, código de segurança, endereço da fatura do cartão e seu C.E.P., a Cidade, o Estado, telefone fixo e telefone celular, além do endereço de e-mail do comprador.<br>
+						Todo pagamento efetuado pelo Usuário e/ou Convidados será encaminhado um e-mail informando acerca do acesso como usuário e/ou por Cotas de presentes virtuais adquiridos por convidado, e aos Convidados em confirmação pelo presente adquirido;<br>
+						O Gift4Us oferece a disponibilização no website, um extrato completo e detalhado dos presentes adquiridos pelos Convidados, os valores disponíveis para saque e os valores bloqueados para transferência bem como as mensagens dos Convidados ao aniversariante. <br>
+						Os valores relacionados à compra de presentes virtuais por parte dos convidados, realizados por meio de cartão de crédito e ou boleto bancário, ficarão disponíveis para conferência no painel de controle dos Usuários imediatamente após a confirmação do pagamento do "presente" pelo Convidado, sendo certo que o valor referente ao presente estará disponível para saque em conta-corrente do Usuário, se dará todos os dias 5 (cinco), 15 (quinze) e 25 (vinte e cinco) de cada mês, caso algum desses dias não seja um dia útil os valores estarão disponíveis para saque no próximo dia útil subsequente.<br>
+						É responsabilidade dos Usuários solicitar o saque dos valores referente aos presentes virtuais recebidos através do painel de controle. Os Usuários deverão informar ao portal gift4us.com.br os dados referente a sua conta-corrente ou conta-poupança para que seja realizado o depósito.<br>
+						O Gift4Us se isenta de informações bancárias informadas pelos usuários de forma equivocada.<br>
+						Salvo disposição em contrário, todas as taxas são cotadas em Reais (R$). O Usuário é o responsável por informar quem irá arcar com pagamento de todas as taxas e impostos associados com o uso da lista de presentes virtuais, podendo repassar este custo referente a taxas, no valor pago pelo presente contido na lista de presentes, para o seu convidado.<br>
+						Para os fins deste Termo de Uso, considera-se dia útil o dia útil bancário na Cidade de São Paulo, Estado de São Paulo.<br>
+						Se após a confirmação pela administradora do cartão do pagamento feito pelo Convidado, o Convidado cancele a transação do cartão junto à administradora e o presente já tiver sido liberado para os Usuários, os Usuários serão informados sobre referido cancelamento e deverão devolver ao Gift4Us o valor total do "presente" recebido decorrente da transação cancelada pelo Convidado. Mesmo em caso de cancelamento, as taxas referentes à contratação do serviço de listas de presentes virtuais do Gift4Us são devidas e deverão ser pagas integralmente.<br>
+						Caso uma lista seja retirada do portal pelo Gift4Us devido à violação dos Termos de Uso, da violação de normas legais ou por ordem judicial, os valores pagos pelos Convidados não serão reembolsados para os Usuários, a não ser que o Gift4Us entenda, a seu critério exclusivo, que a restituição seja apropriada.<br>
+						Tendo em vista que o serviço de meios de pagamento online e de cartões de crédito são independentes do Gift4Us, o próprio Gift4Us não pode dar nenhuma garantia em relação aos pagamentos processados pelo Paypal ou PagSeguro, nem pode responder por falhas de cartões de crédito originadas nesses serviços.<br>
+						Além disso, todas as transações realizadas via PagSeguro são de responsabilidade única e exclusiva do titular da conta PagSeguro, devendo este zelar pela segurança de suas informações, bem como de seu login e senha. O Gift4Us não se responsabilizará pela má utilização dessas informações, tão pouco por transações efetivadas e posteriormente não reconhecidas pelo titular.<br>
+						Os Termos e Condições de Uso aqui descritos são regidos pela legislação da República Federativa do Brasil. Seu texto deverá ser interpretado no idioma português e os visitantes do Portal se submetem ao Foro da Comarca da Capital do Estado de São Paulo.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
 	<div class="clearfix colelem" id="pu2427">
 		<!-- group -->
 		<div class="browser_width ose_ei" id="u2427-bw">
@@ -131,7 +226,10 @@
     				<label>
     					Insira novamente a senha:
     					<input type="password" name="confirma_senha" required>
-    				</label>
+					</label>
+					<label class="" for="checkbox-termos">
+						<input type="checkbox" id="checkbox-termos" class="pull-left"> <span class="pull-left">Li e aceito os <a href="#" data-toggle="modal" data-target="#modal-termos">termos de uso</a></span>
+					</label>
     			</fieldset>
     			<fieldset class="financeiro col-xs-12 col-sm-12 col-md-6 col-lg-6">
     				<span>
@@ -163,7 +261,7 @@
     					<input type="text" name="cpf" value="{{ old('cpf') }}">
     				</label>
     				<span>Fique tranquilo! Você poderá atualizar os dados da sua conta a qualquer momento aqui no portal.</span>
-    			</fieldset>
+				</fieldset>
                 <button type="submit" class="enviar"> Finalizar</button>
               {{ Html::image('assets/site/images/presentinho_red_end.png', '', array('class' => 'presentinho_red')) }}
     		</form>
